4a8c8903cdd67fde7962212015cfdea92bd525aa
lifeadventurer <life0adventurer@gmail.com>
2024-06-02 00:54:21 +0800

    add nvim-autopairs for nvim

---
+++ nvim/lua/lifeadventurer/plugins/autopairs.lua
@@ -0,0 +1,19 @@
+return {
+  "windwp/nvim-autopairs",
+  event = { "InsertEnter" },
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("nvim-autopairs").setup({
+      check_ts = true, -- enable treesitter
+      ts_config = {
+        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+        java = false, -- don't check treesitter on java
+      },
+    })
+    -- make autopairs and completion work together
+    require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+  end,
+}
