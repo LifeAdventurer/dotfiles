03c591a1615febc555b5f7828c7e44d0abc46658
lifeadventurer <life0adventurer@gmail.com>
2024-06-04 19:50:07 +0800

    add formatting.lua for nvim

--- nvim/lua/lifeadventurer/plugins/lsp/mason.lua
+++ nvim/lua/lifeadventurer/plugins/lsp/mason.lua
@@ -2,10 +2,12 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
     mason.setup({
       ui = {
@@ -35,6 +37,14 @@ return {
         "tsserver",
         "yamlls",
       },
+    })
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "black",
+        "isort",
+      }
     })
   end,
 }
