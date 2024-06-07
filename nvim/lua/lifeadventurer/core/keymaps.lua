051978780fad9730a27b67de0331248ba3bc4c28
lifeadventurer <life0adventurer@gmail.com>
2024-06-07 11:48:54 +0800

    nvim/ add resize window keymaps

--- nvim/lua/lifeadventurer/core/keymaps.lua
+++ nvim/lua/lifeadventurer/core/keymaps.lua
@@ -14,6 +14,11 @@ vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split window" })
 
+vim.keymap.set("n", "<C-Up>", "<C-w>+", { desc = "Increase window height by 1" })
+vim.keymap.set("n", "<C-Down>", "<C-w>-", { desc = "Decrease window height by 1" })
+vim.keymap.set("n", "<C-Left>", "<C-w><", { desc = "Increase window width by 1" })
+vim.keymap.set("n", "<C-Right>", "<C-w>>", { desc = "Decrease window width by 1" })
+
 -- tab management
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
