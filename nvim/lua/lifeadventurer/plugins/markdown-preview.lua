3f487ee0c8b45750924f1c3c77764cafbfd5a564
lifeadventurer <life0adventurer@gmail.com>
2024-06-13 17:39:58 +0800

    nvim/ add markdown-preview.nvim

---
+++ nvim/lua/lifeadventurer/plugins/markdown-preview.lua
@@ -0,0 +1,15 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  build = function() vim.fn["mkdp#util#install"]() end,
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  config = function()
+    vim.g.mkdp_auto_start = 0
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_open_ip = '127.0.0.1'
+    vim.g.mkdp_browser = ''
+    vim.g.mkdp_echo_preview_url = 0
+    vim.g.mkdp_port = 8080
+    vim.g.mkdp_page_title = '${name}'
+  end,
+  ft = { "markdown" },
+}
