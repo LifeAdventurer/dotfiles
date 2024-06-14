79a0682f2d22281471bed57a02a79db886c01c03
lifeadventurer <life0adventurer@gmail.com>
2024-06-14 12:41:00 +0800

    nvim/ disable change detection

--- nvim/lua/lifeadventurer/lazy.lua
+++ nvim/lua/lifeadventurer/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "lifeadventurer.plugins"}, { import = "lifeadventurer.plugins.lsp" } })
+require("lazy").setup({ { import = "lifeadventurer.plugins"}, { import = "lifeadventurer.plugins.lsp" } }, {
+  change_detection = {
+    notify = false,
+  },
+})
